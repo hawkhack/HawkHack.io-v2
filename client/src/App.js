49e330bedf48c23aa1013899487b403986a11cbe
Aadil Glasswala <aglasswala@gmail.com>
2019-06-29 16:16:56 -0400

    initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
-import React ,{Component } from 'react';
-import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 // import { defaults } from "./Defaults"; // Get a handle to the default application settings
-import './App.css';
 import LandingPage from "./components/Landing/Landing";
 
 
 
 
-class App extends Component{
+class App extends Component {
 
-render(){
-  return (
-    <BrowserRouter>
+  render() {
+    return (
+      <BrowserRouter>
         <Switch>
           <Route exact path="/" render={(props) => <LandingPage {...props} />} />
         </Switch>
-    </BrowserRouter>
-    
+      </BrowserRouter>
+
     );
   }
 };
--- client/src/components/Landing/Landing.js
+++ client/src/components/Landing/Landing.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
-import "../../App.css";
 import Hero from "./sections/Hero";
 
-export default class LandingPage extends Component {
+class LandingPage extends Component {
     render() {
         return (
             <Hero />
-            
         )
     }
 }
+
+export default LandingPage
