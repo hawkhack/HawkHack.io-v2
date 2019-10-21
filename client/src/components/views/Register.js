242ea24b53bbcef61cac2b489b55eec84434abb4
no-vac <crysist14@gmail.com>
2019-10-21 16:41:42 -0400

    fixed register form layout

--- client/src/components/views/Register.js
+++ client/src/components/views/Register.js
@@ -1,41 +1,40 @@
-import React, { Component, Fragment } from 'react' 
+import React, { Component, Fragment } from "react";
 
-import { Grid, Paper, withStyles, Typography } from '@material-ui/core'
+import { Grid, Paper, withStyles, Typography } from "@material-ui/core";
 
-import registerStyles from '../styles/registerStyles'
-import RegisterForm from '../forms/RegisterForm'
+import registerStyles from "../styles/registerStyles";
+import RegisterForm from "../forms/RegisterForm";
 
 class Register extends Component {
-
-	render() {
-		const { classes } = this.props
-		return (
-			<Fragment>
-				<div className={classes.image}>
-					<div className={classes.grad}>
-					    <Grid
-					        container
-					        direction="row"
-					        justify="center"
-					        alignItems="center"
-					        style={{height: "100vh"}}
-					    >
-					        <Grid item xs sm={12} md={6}>
-							    <Typography variant="h1" align="left" color="secondary">
-				                    Register
-			                	</Typography>
-					            <Paper className={classes.paper}>
-					            	<div className={classes.outer}>
-						                <RegisterForm />
-						            </div>
-					            </Paper>
-					        </Grid>
-					    </Grid>
-					</div>
-				</div>
-			</Fragment>
-		)
-	}
+  render() {
+    const { classes } = this.props;
+    return (
+      <Fragment>
+        <div className={classes.image}>
+          <div className={classes.grad}>
+            <Grid
+              container
+              direction="row"
+              justify="center"
+              alignItems="center"
+              style={{ height: "100vh" }}
+            >
+              <Grid item xs sm={12} md={6}>
+                <Typography variant="h1" align="left" color="secondary">
+                  Register
+                </Typography>
+                <Paper className={classes.paper}>
+                  <div className={classes.outer}>
+                    <RegisterForm />
+                  </div>
+                </Paper>
+              </Grid>
+            </Grid>
+          </div>
+        </div>
+      </Fragment>
+    );
+  }
 }
 
-export default withStyles(registerStyles)(Register)
+export default withStyles(registerStyles)(Register);
