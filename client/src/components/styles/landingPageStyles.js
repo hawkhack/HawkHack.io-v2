08e74cee57ded29e07e2fb128d606b02be2de3ce
Aadil Glasswala <aglasswala@gmail.com>
2020-01-26 17:16:35 -0500

    testing on mobile

--- client/src/components/styles/landingPageStyles.js
+++ client/src/components/styles/landingPageStyles.js
@@ -51,7 +51,21 @@ const landingPageStyles = makeStyles((theme) => ({
 	  	zIndex: "3"
 	},
 	mainRaised: {
-	  	margin: "-60px 30px 0px",
+		"@media (min-width: 276px)": {
+			margin: "-60px 10px 0px",
+		},
+	  	"@media (min-width: 576px)": {
+	  		margin: "-60px 10px 0px",
+	  	},
+	  	"@media (min-width: 768px)": {
+	  	    margin: "-60px 20px 0px",
+	  	},
+	  	"@media (min-width: 992px)": {
+	  	    margin: "-60px 30px 0px",
+	  	},
+	  	"@media (min-width: 1200px)": {
+	  	    margin: "-60px 30px 0px",
+	  	},
 	  	borderRadius: "6px",
 	  	boxShadow:
 	    	"0 16px 24px 2px rgba(0, 0, 0, 0.14), 0 6px 30px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2)"
