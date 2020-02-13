296d20f5a283256448a768d5a574d9dc3e0976ae
Cristian Novac <crysist14@gmail.com>
2020-02-12 21:17:24 -0500

    changed scripts to pull and start with pm2

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -e
 
+echo "pulling changes from repo"
+git pull
+
 file_exists() {
   if [ ! -f "$1" ]; then
     echo "File $1 is missing. Please acquire it from another developer."
@@ -12,26 +15,29 @@ file_exists() {
 
 cd client || exit 1
 rm -rf node_modules
-echo "Removed node_modules"
+echo "Removed client node_modules"
 
-echo "Checking if the configuration files are setup..."
+echo "Checking if the client configuration files are setup..."
 file_exists ".env"
+echo "All client configuration files in client exist!"
 
-echo "All configuration files in client exist!"
-
+echo "Installing client packages"
 npm i 
 echo "Make sure to 'npm start' the front end"
 cd ..
 
+echo "Removing server node_modules"
 rm -rf node_modules
-echo "Removed node_modules"
 
-echo "Checking if the configuration files are setup..."
+echo "Checking if the server configuration files are setup..."
 cd config
 file_exists ".env"
 cd ..
 
-echo "All configuration files exist!"
+echo "All server configuration files exist!"
 
+echo "Installing server packages"
 npm i
-npm start
+
+echo "starting with PM2"
+pm2 start npm -- run -- server
--- scripts/start-prod.sh
+++ scripts/start-prod.sh
@@ -3,6 +3,9 @@
 
 set -e
 
+echo "pulling changes from repo"
+git pull
+
 file_exists() {
   if [ ! -f "$1" ]; then
     echo "File $1 is missing. Please acquire it from another developer."
@@ -16,25 +19,31 @@ cd client || exit 1
 rm -rf node_modules
 echo "Removed node_modules"
 
-echo "Checking if the configuration files are setup..."
+echo "Checking if the client configuration files are setup..."
 file_exists ".env"
 
-echo "All configuration files in client exist!"
+echo "All client configuration files in client exist!"
 
+echo "Installing client packages"
 npm ci 
+
+echo "Building react app"
 npm run build
 
 cd ..
 
+echo "Removing server node_modules"
 rm -rf node_modules
-echo "Removed node_modules"
 
-echo "Checking if the configuration files are setup..."
+echo "Checking if the server configuration files are setup..."
 cd config
 file_exists ".env"
 cd ..
 
-echo "All configuration files exist!"
+echo "All server configuration files exist!"
 
+echo "Installing server packages"
 npm ci
-npm start
+
+echo "Starting with PM2"
+pm2 start npm -- start
